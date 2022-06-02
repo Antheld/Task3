f99a1d75d754f9ae3c0821d687c43e3481e87ce5
Antheld <89795541+Antheld@users.noreply.github.com>
2022-06-02 16:12:50 +0300

    Добавьте файлы проекта.

---
+++ Task3/Task3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int* diopters = new int[N];
+    for (int i = 0; i < N; ++i) {
+        cin >> diopters[i];
+    }
+
+    int temp;
+    for (int i = 0; i < N - 1; ++i) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (diopters[j] > diopters[j + 1]) {
+                temp = diopters[j + 1];
+                diopters[j + 1] = diopters[j];
+                diopters[j] = temp;
+            }
+        }
+    }
+
+    int count = N;
+    for (int i = 0; i < N - 1; ++i) {
+        if (diopters[i + 1] - diopters[i] <= 2) {
+            count--;
+            i++;
+        };
+    }
+
+    cout << count;
+
+    delete[] diopters;
+}
